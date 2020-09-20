6f7ea0bb43e641c6fa62831d70e2e472785ad078
leonardo <leon.ak@hotmail.com>
2020-09-20 19:29:57 -0300

    add endpoints constant file

--- server/constants/index.ts
+++ server/constants/index.ts
@@ -3,5 +3,6 @@ import errors from './errors';
 import headers from './headers';
 import messages from './messages';
 import values from './values';
+import endpoints from './endpoints';
 
-export { cryptography, errors, headers, messages, values };
+export { cryptography, errors, headers, messages, values, endpoints };
