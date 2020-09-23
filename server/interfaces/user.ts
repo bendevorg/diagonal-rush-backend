83e3701089f4c4d6f23f6df9bdf48df1f47423ca
unknown <guibasconti@gmail.com>
2020-09-22 21:48:12 -0300

    Add chapter schema

--- server/interfaces/user.ts
+++ server/interfaces/user.ts
@@ -1,10 +1,12 @@
 import { Document } from 'mongoose';
 import { IDevice } from './device';
+import { IChapter } from './chapter';
 
 export interface IUserModel extends Document {
   _id: string;
   device: IDevice;
   points: number;
+  chapters: Array<IChapter>;
   createdAt?: Date;
   updatedAt?: Date;
 }
